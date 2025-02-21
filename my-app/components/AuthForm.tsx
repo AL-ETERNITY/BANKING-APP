9fe059a2a84601eb29bbdda0bede2aa62e568876
Siddhant Shah <siddhantshah469@gmail.com>
2025-02-22 03:31:55 +0530

    Update package dependencies; add Footer component and integrate user session management

--- my-app/components/AuthForm.tsx
+++ my-app/components/AuthForm.tsx
@@ -17,71 +17,67 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import CustomInput from './CustomInput';
-import { authFormSchema } from '@/lib/utils';
-import { Loader2 } from 'lucide-react';
-import { useRouter } from 'next/navigation';
+import CustomInput from "./CustomInput";
+import { authFormSchema } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { signIn, signUp } from "@/lib/actions/user.actions";
+// import PlaidLink from "./PlaidLink";
 // import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
 // import PlaidLink from './PlaidLink';
 
 
 const AuthForm = ({ type }: { type: string }) => {
-    const router = useRouter();
-    const [user, setUser] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const formSchema = authFormSchema(type);
+  const formSchema = authFormSchema(type);
 
-    // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-      resolver: zodResolver(formSchema),
-      defaultValues: {
-        email: "",
-        password: ''
-      },
-    })
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      firstName: "",
+      lastName: "",
+      address1: "",
+      city: "",
+      state: "",
+      postalCode: "",
+      dateOfBirth: "",
+      ssn: "",
+    },
+  });
 
-      // 2. Define a submit handler.
-      const onSubmit = async (data: z.infer<typeof formSchema>) => {
-        // setIsLoading(true);
+  // 2. Define a submit handler.
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    setIsLoading(true);
 
-        // try {
-          // Sign up with Appwrite & create plaid token
+    try {
+      // Sign up with Appwrite & create plaid token
 
-        //   if(type === 'sign-up') {
-        //     const userData = {
-        //       firstName: data.firstName!,
-        //       lastName: data.lastName!,
-        //       address1: data.address1!,
-        //       city: data.city!,
-        //       state: data.state!,
-        //       postalCode: data.postalCode!,
-        //       dateOfBirth: data.dateOfBirth!,
-        //       ssn: data.ssn!,
-        //       email: data.email,
-        //       password: data.password
-        //     }
+      if (type === "sign-up") {
+        const newUser = await signUp(data);
 
-        //     const newUser = await signUp(userData);
-
-        //     setUser(newUser);
-        //   }
-
-        //   if(type === 'sign-in') {
-        //     const response = await signIn({
-        //       email: data.email,
-        //       password: data.password,
-        //     })
-
-        //     if(response) router.push('/')
-        //   }
-        // } catch (error) {
-        //   console.log(error);
-        // } finally {
-        //   setIsLoading(false);
-        // }
+        setUser(newUser);
       }
+
+      if (type === "sign-in") {
+        const response = await signIn({
+          email: data.email,
+          password: data.password,
+        });
+
+        if (response) router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <section className="auth-form">
